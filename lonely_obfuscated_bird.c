73711197c6fc3ebe0d3faf701ce98855fd07a492
Mevio <61273845+torotonnato@users.noreply.github.com>
2023-01-20 18:02:00 +0100

    Delete lonely_obfuscated_bird.c

--- lonely_obfuscated_bird.c
+++
@@ -1,2 +0,0 @@
-#include <math.h>
-main(f,i,s,o){for (i=o=0;;putchar(s=o>0?127*sin(.43*i-750*cos(142e-6*f*i)/f):0))if(o*o>1)o>1?o--:o++,i++;else o=o>0?-rand()%20000-8000:rand()%10000+2000,f=rand()%10+8;}
